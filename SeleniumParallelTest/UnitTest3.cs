06dc2f4b79221dde1917967c17063cff10bf39d6
daretodreamz <bing.wang2016@hotmail.com>
2016-09-27 17:31:21 +1300

    wait.Until usages

--- SeleniumParallelTest/UnitTest3.cs
+++ SeleniumParallelTest/UnitTest3.cs
@@ -17,6 +17,7 @@ namespace SeleniumParallelTest
         {
             
         }
+
         [Test]
         public void FirefoxTradeMeTest()
         {
@@ -25,14 +26,54 @@ namespace SeleniumParallelTest
 
             IWait<IWebDriver> wait = new WebDriverWait(Driver, TimeSpan.FromMilliseconds(3000));
 
-            wait.Until(drv => ((IJavaScriptExecutor)drv).ExecuteScript("return document.readyState").Equals("complete"));
+            wait.Until<bool>(
+                delegate(IWebDriver drv)
+                {
+                    IJavaScriptExecutor js = drv as IJavaScriptExecutor;
+                    return js.ExecuteScript("return document.readyState").Equals("complete");
+                }
+            );
+            
+            ////////wait.Until(drv => ((IJavaScriptExecutor)drv).ExecuteScript("return document.readyState").Equals("complete"));
 
-            ////////IJavaScriptExecutor js = Driver as IJavaScriptExecutor;
-            ////////string title = js.ExecuteScript("return document.readyState").ToString();
+            ////////wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("An Object ID")));
+            
+            //////////wait.Until(ExpectedConditionsExtension.ProcessBarDisappears());
+
+            ////////////////////IJavaScriptExecutor js = Driver as IJavaScriptExecutor;
+            ////////////////////string title = js.ExecuteScript("return document.readyState").ToString();
 
             ITakesScreenshot screenshotDriver = Driver as ITakesScreenshot;
             Screenshot screenshot = screenshotDriver.GetScreenshot();
             screenshot.SaveAsFile(@"D:\IECapture\TradeMe.jpg", ImageFormat.Jpeg);
         }
     }
+
+
+    /// <summary>
+    /// 自定义的扩展条件
+    /// </summary>
+    public class ExpectedConditionsExtension
+    {
+        /// <summary>
+        /// 等待进度条消失
+        /// </summary>
+        /// <param name="driver">WebDriver对象</param>
+        /// <returns>操作Func对象</returns>
+        public static Func<IWebDriver, bool> ProcessBarDisappears()
+        {
+            return delegate (IWebDriver driver)
+            {
+                IWebElement element = null;
+                try
+                {
+                    element = driver.FindElement(By.XPath(".//div[@id='divProcessBar']"));
+                }
+                catch (NoSuchElementException) { return true; }
+                return !element.Displayed;
+            };
+        }
+    }
+
+
 }
